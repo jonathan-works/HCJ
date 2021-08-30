eae2382cd73b0659ec57ba0259cf27f2a84e980c
Jonathan Santana <jonathan-works@hotmail.com>
2021-08-30 09:33:37 -0300

    Add - Mapa, Personagem e Acoes

--- Assets/Scripts/PersonagemControoller.cs
+++ Assets/Scripts/PersonagemControoller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PersonagemControoller : MonoBehaviour
 {
     public Animator personagemAnimator;
@@ -9,10 +10,15 @@ public class PersonagemControoller : MonoBehaviour
     float input_y = 0;
     public float velocidade = 2.5f;
     bool esta_andando = false;
+
+    Rigidbody2D rb2D;
+    Vector2 movimento = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         esta_andando = false;
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,14 +29,23 @@ public class PersonagemControoller : MonoBehaviour
         
         esta_andando = (input_x !=0 || input_y != 0);
 
+        movimento = new Vector2(input_x, input_y);
+
         if (esta_andando)
         {
-            var movimento = new Vector3(input_x, input_y, 0).normalized;
-            transform.position += movimento * velocidade * Time.deltaTime;
             personagemAnimator.SetFloat("input_x", input_x);
             personagemAnimator.SetFloat("input_y", input_y);
         }
         personagemAnimator.SetBool("esta_andando", esta_andando);
 
+        if(Input.GetButtonDown("Fire1")){
+            personagemAnimator.SetTrigger("ataque");
+        }
+
+    }
+
+    private void FixedUpdate() 
+    {
+        rb2D.MovePosition(rb2D.position + movimento * velocidade * Time.fixedDeltaTime);
     }
 }
